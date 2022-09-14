c051174972f94198d0a69f71dfb10b3a8077c88e
dbroemme <darren.broemmer@gmail.com>
2022-09-14 19:23:30 -0400

    Add a quit button

--- src/Screens/PvPButton.gd
+++ src/Screens/PvPButton.gd
@@ -2,9 +2,13 @@ extends Button
 
 export(bool) var vs_computer
 export(bool) var easy_mode
+export(bool) var quit_game
 
 
 func _on_button_up():
+	if quit_game:
+		get_tree().quit()
+
 	GameSingleton.vs_computer = vs_computer
 	GameSingleton.easy_mode = easy_mode
 	get_tree().change_scene("res://src/Screens/Board.tscn")
